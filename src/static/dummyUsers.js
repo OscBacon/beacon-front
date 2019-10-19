f010cb2f03a58696af08b7548579c1dcdfda9a26
Oscar Baracos <oscar.baracos@gmail.com>
2019-10-19 13:35:11 -0400

    Update dummyUser to use first and last name

--- src/static/dummyUsers.js
+++ src/static/dummyUsers.js
@@ -1,3 +1,3 @@
 export default [
-    {name: "Amir Mousavi", username: "Amir", avatar: "https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-1/p160x160/62357713_2486535374699741_4082721466909458432_n.jpg?_nc_cat=110&_nc_oc=AQnzKrIBP2CZlC42tG23jPnCM0o0bdIyvuyyH4Ngotnq-wlwix0cU_ttaRq6oSIfugg&_nc_ht=scontent-yyz1-1.xx&oh=524fe57b6cd9f19183887c60713bcb1e&oe=5E2BD0FA"}
+    {firstName: "Amir", lastName: "Mousavi", username: "Amir", avatar: "https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-1/p160x160/62357713_2486535374699741_4082721466909458432_n.jpg?_nc_cat=110&_nc_oc=AQnzKrIBP2CZlC42tG23jPnCM0o0bdIyvuyyH4Ngotnq-wlwix0cU_ttaRq6oSIfugg&_nc_ht=scontent-yyz1-1.xx&oh=524fe57b6cd9f19183887c60713bcb1e&oe=5E2BD0FA"}
 ]
